21822e5deb22cbc8d6a96fb08ce828e28a4a3e8f
Carlos Abella <abella@lhr.local>
2018-12-24 20:08:11 +0100

    some refactor in variables

--- emailAPI.py
+++ emailAPI.py
@@ -7,8 +7,8 @@ sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 from_email = Email(emailaddress)
 to_email = Email(emailaddress)
 
-def sendEmail(cont, dt, user):
-    subject = user + " Twit Alert " + "(" + dt.__str__() +")"
+def sendEmail(cont, dt, membername):
+    subject = membername + " Twit Alert " + "(" + dt + ")"
     content = Content("text/plain", cont)
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
